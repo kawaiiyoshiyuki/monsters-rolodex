4672e64b1af1c20526add69c2f90697ffe4e0e85
habr <jonas.habr@t-mobile.cz>
2019-11-16 14:52:41 +0100

    Updates

---
+++ precache-manifest.ed5bc2297bf294efafb7031ce8ca3d3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5c688700d14daaa0c91ce74f93e568",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4357fccf73975c18442d",
+    "url": "/monsters-rolodex/static/css/main.96c3d69a.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "4357fccf73975c18442d",
+    "url": "/monsters-rolodex/static/js/main.ad0833dd.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
